49aaea66678b8afc15b8bf41a806a5b5fdceeeb4
Thomas Munoz <thomasmunoz30@gmail.com>
2014-10-30 02:32:50 +0100

    Add panel in pure CSS
    With a little bit of jQuery

--- src/front-end/js/main.js
+++ src/front-end/js/main.js
@@ -3,11 +3,21 @@ $(document).ready(function() {
 	// Change the google map element take the width of the window
 	$('#map').width($(window).width());
 
-	/* Everytime you change the size of the window, it changes the google
-	* map width
-	*/
-	
+	// margin:auto for search-panel and search-panel-date
+	var leftMargin = ($(window).width() - $('#search-panel').width()) / 2;
+	$('#search-panel').css('left', leftMargin);
+	$('#search-panel-date').css('left', (leftMargin + 5));
+
+	// Everytime you change the size of the window
 	$( window ).resize(function() {
-  		$('#map').width($(window).width());
+
+		// Change the google map size
+	  	$('#map').width($(window).width());
+
+	  	// margin:auto for search-panel and search-panel-date
+	  	var leftMargin = ($(window).width() - $('#search-panel').width()) / 2;
+	  	$('#search-panel').css('left', leftMargin);
+	  	$('#search-panel-date').css('left', (leftMargin + 5));
+
 	});
 });
