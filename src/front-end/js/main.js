6538aef066530161193a8c72c5c58e20af7c91bc
Thomas Munoz <thomasmunoz30@gmail.com>
2014-11-01 15:58:43 +0100

    add fixed width & correct colors
    #SWAG

--- src/front-end/js/main.js
+++ src/front-end/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	
 	// Change the google map element take the width of the window
-	$('#map').width($(window).width());
+	$('#map').width($('body').width());
 
 	// margin:auto for search-panel and search-panel-date
 	var leftMargin = ($(window).width() - $('#search-panel').width()) / 2;
@@ -12,7 +12,7 @@ $(document).ready(function() {
 	$( window ).resize(function() {
 
 		// Change the google map size
-	  	$('#map').width($(window).width());
+	  	$('#map').width($('body').width());
 
 	  	// margin:auto for search-panel and search-panel-date
 	  	var leftMargin = ($(window).width() - $('#search-panel').width()) / 2;
