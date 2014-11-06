b236165e9bf6dbcb092e28d3cce21d4e864636a6
sknz <florandara@gmail.com>
2014-11-06 11:11:11 +0100

    CRUD/missing files

---
+++ src/back-end/SharpDrift/SharpDrift/DataModel/Campus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpDrift.DataModel
+{
+    public class Campus
+    {
+        public int Id { get; set; }
+        public String Address { get; set; }
+        public String Nom { get; set; }
+    }
+}
---
+++ src/back-end/SharpDrift/SharpDrift/Modules/CampusModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Security;
+using Insight.Database;
+using Insight.Database.Json;
+using SharpDrift.Utilities;
+using SharpDrift.DataModel;
+
+namespace SharpDrift.Modules
+{
+    public class CampusModule : NancyModule
+    {
+        public CampusModule()
+        {
+            this.RequiresAuthentication();
+
+            Get["/campus", true] = async (_, ctx) => 
+                {
+                    using (var conn = DAL.Conn)
+                    {
+                        IList<Campus> campus = await conn.QuerySqlAsync<Campus>("SELECT * FROM CAMPUS");
+                        return new JsonNetObjectSerializer().SerializeObject(campus.GetType(), campus);
+                    }
+                };
+        }
+    }
+}
