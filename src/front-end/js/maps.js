2bbef2473453a5fc23aaa113a424586fe0cea527
Thomas Munoz <thomas.munoz30@gmail.com>
2014-11-14 18:58:51 +0100

    Maj MapController with new #SWAG functions
    Next : unit testing

--- src/front-end/js/maps.js
+++ src/front-end/js/maps.js
@@ -3,51 +3,86 @@
 
 var myApp = angular.module('Amudrive', []);
 
-myApp.controller('MapController', function($scope){
+myApp.controller('MapController', ['$scope', mapController])
+
+function mapController ($scope){
+
+    // Informations de bases pour la map (par défaut)
     $scope.zoom = 15;
     $scope.mapTypeId = google.maps.MapTypeId.ROADMAP;
     $scope.defaultLocation = [43.529742, 5.447427];
 
+    // Coordonnées du point courant
+    $scope.loc = ($scope.loc == undefined) ? $scope.defaultLocation : $scope.loc;
 
-    $scope.createMap = function(loc){
+    // Tableau de marqueurs
+    $scope.tabMarker = [];
 
-        if($scope.loc == undefined && loc == undefined)
-            $scope.loc = $scope.defaultLocation;
 
-        else if (loc != $scope.defaultLocation && loc != undefined && $scope.loc == undefined || $scope.loc == $scope.defaultLocation)
-            $scope.loc = [loc[0], loc[1]];
 
-        else if($scope.loc != $scope.defaultLocation && loc != undefined)
-            $scope.loc = [loc[0], loc[1]];
-
-        else
-            $scope.loc = $scope.defaultLocation;
-
+    $scope.displayMap = function(){
+        console.log('DisplayMap : ' + $scope.loc);
         var mapOptions = {
             zoom: $scope.zoom,
             center: new google.maps.LatLng($scope.loc[0], $scope.loc[1]),
             mapTypeId: $scope.mapTypeId
-        }
+        };
 
         $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-       var infoWindow = new google.maps.InfoWindow();
-        var marker = new google.maps.Marker({
-            map: $scope.map,
-            position: new google.maps.LatLng($scope.loc[0], $scope.loc[1]),
-            visible: true,
-            title : "Bienvenue à "
-        });
+    };
 
-        marker.content = '<div class="infoWindowContent"> #SWAG land !</div>';
+    $scope.displayMap();
+    /* Ajoute un marqueur sur la map*/
 
-        google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-            infoWindow.open($scope.map, marker);
-        });
+    $scope.showMarker = function(){
 
-    }
-});
+        if($scope.tabMarker.length < 1)
+            return false;
+
+        console.log($scope.tabMarker);
+
+        for(var i = 0; i < $scope.tabMarker.length; i += 2) {
+            var loc = $scope.tabMarker[i+1];
+            var content = $scope.tabMarker[i];
+
+            console.log('loc : ' + loc + ' content : ' + content);
+            var infoWindow = new google.maps.InfoWindow();
+
+            var marker = new google.maps.Marker({
+                map: $scope.map,
+                position: new google.maps.LatLng(loc[0], loc[1]),
+                visible: true,
+                title: content[0]
+            });
+
+            marker.content = '<div class="infoWindowContent">' + content[1] + '</div>';
+
+            google.maps.event.addListener(marker, 'click', function () {
+                infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                infoWindow.open($scope.map, marker);
+            });
+        }
+    };
+
+    // Ajoute un marqueur à la liste des marqueurs courant
+    $scope.addMarker = function(content, loc){
+        if(content != undefined && loc != undefined)
+            $scope.tabMarker.push(content, loc);
+
+        $scope.showMarker();
+    };
+
+    $scope.changeZoom = function(zoom){
+        $scope.zoom = (zoom == undefined) ? $scope.zoom : zoom;
+        $scope.displayMap();
+    };
+
+    $scope.changeLocation = function(loc) {
+        $scope.loc = (loc == undefined) ? $scope.loc : loc;
+        $scope.displayMap();
+    };
+}
 
 /* Directives */
 
@@ -56,7 +91,6 @@ myApp.directive('googlePlaces', function(){
             // Remplace l'input google-places par un nouveau (dynamique, permettant l'autocomplete)
             restrict:'EA',
             replace:true,
-            scope: {location:'='},
             template: '<input id="google_places_ac" name="google_places_ac" type="text" class="input-block-level searchMap"/>',
             controller: 'MapController',
 
@@ -75,8 +109,9 @@ myApp.directive('googlePlaces', function(){
                     var loc = [place.geometry.location.lat(),place.geometry.location.lng()];
 
                     // Chargement de la map
-                    $scope.createMap(loc);
-
+                    $scope.changeLocation(loc);
+                    $scope.addMarker(['Bienvenue', 'Bonjour !'], loc);
+                    //$scope.testMarker();
                     // Applique les modification du scope et recharge les éléments modifiés
                     $scope.$apply();
                 });
