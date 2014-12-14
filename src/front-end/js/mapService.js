0d8d2d11fd8e9a5cc948854156f66ef0443d700a
Thomas Munoz <thomas.munoz30@gmail.com>
2014-12-14 22:57:18 +0100

    BOOM ! MapService is on the stage !
    BOOM #SWAG COMMIT YOU FOOL !

---
+++ src/front-end/js/mapService.js
@@ -0,0 +1,138 @@
+myApp.service('Map', function(){
+    //To avoid this context conflicts
+    that = this;
+
+    // Informations de bases pour la map (par défaut)
+    that.zoom = 15;
+    that.mapTypeId = google.maps.MapTypeId.ROADMAP;
+    that.defaultLocation = [43.529742, 5.447427];
+
+
+    // Coordonnées du point courant
+    that.loc = (that.loc == undefined) ? that.defaultLocation : that.loc;
+
+    // Tableau de marqueurs
+    that.tabMarker = [];
+
+
+
+    that.displayMap = function(){
+        console.log('DisplayMap : ' + that.loc);
+        var mapOptions = {
+            zoom: that.zoom,
+            center: new google.maps.LatLng(that.loc[0], that.loc[1]),
+            mapTypeId: that.mapTypeId
+        };
+
+        that.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        that.directionsDisplay = new google.maps.DirectionsRenderer(
+            {
+                'map' : that.map
+            });
+
+    };
+
+    that.directionsService = new google.maps.DirectionsService();
+
+    /* Ajoute un marqueur sur la map*/
+    that.showMarker = function(){
+
+        if(that.tabMarker.length < 1)
+            return false;
+
+        console.log(that.tabMarker);
+
+        for(var i = 0; i < that.tabMarker.length; i += 2) {
+            var loc = that.tabMarker[i+1];
+            var content = that.tabMarker[i];
+
+            console.log('loc : ' + loc + ' content : ' + content);
+
+            var infoWindowContent = '<div class="infoWindow"><h1>' + content[0] + '</h1>'
+                + '<hr/> ' + content[1] + '</div>';
+
+
+            var infoWindow = new google.maps.InfoWindow({
+                content: infoWindowContent
+            });
+
+            var marker = new google.maps.Marker({
+                map: that.map,
+                position: new google.maps.LatLng(loc[0], loc[1]),
+                visible: true,
+                title: content[0]
+            });
+
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infoWindow.setContent(infoWindowContent);
+                    infoWindow.open(that.map, marker);
+                }
+            })(marker, i));
+        }
+    };
+
+    // Ajoute un marqueur à la liste des marqueurs courant
+    that.addMarker = function(content, loc){
+        if(content != undefined && loc != undefined)
+            that.tabMarker.push(content, loc);
+
+        that.showMarker();
+    };
+
+    that.changeZoom = function(zoom){
+        that.zoom = (zoom == undefined) ? that.zoom : zoom;
+        that.displayMap();
+    };
+
+    that.changeLocation = function(loc) {
+        that.loc = (loc == undefined) ? that.loc : loc;
+        that.displayMap();
+    };
+
+    that.drawCircle = function(loc, radius){
+        that.circle = new google.maps.Circle(
+            {
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#FF0000',
+                fillOpacity: 0.35,
+                map: that.map,
+                center: new google.maps.LatLng(loc[0], loc[1]),
+                radius: radius * 1000
+            }
+        );
+    };
+
+    that.changeCircleRadius = function(radius){
+        that.circle.setRadius(radius * 1000);
+    };
+
+    that.getRadius = function(){
+        return that.circle.getRadius();
+    };
+
+    that.computeRoute = function(){
+        var dep = new google.maps.LatLng(that.loc[0], that.loc[1]);
+        var dest = new google.maps.LatLng(that.defaultLocation[0], that.defaultLocation[1]);
+        var request = {
+            origin: dep,
+            destination: dest,
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.METRIC
+        };
+
+        that.directionsService.route(request, function(result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                that.directionsDisplay.setDirections(result);
+                that.directionsDisplay.suppressMarkers = true;
+                that.directionsDisplay.setOptions({
+                    markerOptions : google.maps.Animation.BOUNCE,
+                    preserveViewport : false
+                });
+            }
+        });
+    };
+});
--- src/front-end/js/maps.js
+++ src/front-end/js/maps.js
@@ -1,160 +1,21 @@
 'use strict';
 
+myApp.controller('MapController', ['$scope','Map', mapController]);
 
-var myApp = angular.module('Amudrive', ['ngRoute']);
-
-myApp.controller('MapController', ['$scope', mapController]);
 myApp.factory('MapService', function ($rootScope) {
     var sharedService = {};
 
     sharedService.message = '';
     sharedService.addressToCoordRequest
 });
-
-function mapController ($scope){
-
-    // Informations de bases pour la map (par défaut)
-    $scope.zoom = 15;
-    $scope.mapTypeId = google.maps.MapTypeId.ROADMAP;
-    $scope.defaultLocation = [43.529742, 5.447427];
-
-
-    // Coordonnées du point courant
-    $scope.loc = ($scope.loc == undefined) ? $scope.defaultLocation : $scope.loc;
-
-    // Tableau de marqueurs
-    $scope.tabMarker = [];
-
-
-
-    $scope.displayMap = function(){
-        console.log('DisplayMap : ' + $scope.loc);
-        var mapOptions = {
-            zoom: $scope.zoom,
-            center: new google.maps.LatLng($scope.loc[0], $scope.loc[1]),
-            mapTypeId: $scope.mapTypeId
-        };
-
-        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-        $scope.directionsDisplay = new google.maps.DirectionsRenderer(
-            {
-                'map' : $scope.map
-            });
-
-    };
-
-    $scope.displayMap();
-
-    $scope.directionsService = new google.maps.DirectionsService();
-
-    /* Ajoute un marqueur sur la map*/
-    $scope.showMarker = function(){
-
-        if($scope.tabMarker.length < 1)
-            return false;
-
-        console.log($scope.tabMarker);
-
-        for(var i = 0; i < $scope.tabMarker.length; i += 2) {
-            var loc = $scope.tabMarker[i+1];
-            var content = $scope.tabMarker[i];
-
-            console.log('loc : ' + loc + ' content : ' + content);
-
-            var infoWindowContent = '<div class="infoWindow"><h1>' + content[0] + '</h1>'
-                                    + '<hr/> ' + content[1] + '</div>';
-
-
-            var infoWindow = new google.maps.InfoWindow({
-                content: infoWindowContent
-            });
-
-            var marker = new google.maps.Marker({
-                map: $scope.map,
-                position: new google.maps.LatLng(loc[0], loc[1]),
-                visible: true,
-                title: content[0]
-            });
-
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                return function() {
-                    infoWindow.setContent(infoWindowContent);
-                    infoWindow.open($scope.map, marker);
-                }
-            })(marker, i));
-        }
-    };
-
-    // Ajoute un marqueur à la liste des marqueurs courant
-    $scope.addMarker = function(content, loc){
-        if(content != undefined && loc != undefined)
-            $scope.tabMarker.push(content, loc);
-
-        $scope.showMarker();
-    };
-
-    $scope.changeZoom = function(zoom){
-        $scope.zoom = (zoom == undefined) ? $scope.zoom : zoom;
-        $scope.displayMap();
-    };
-
-    $scope.changeLocation = function(loc) {
-        $scope.loc = (loc == undefined) ? $scope.loc : loc;
-        $scope.displayMap();
-    };
-
-    $scope.drawCircle = function(loc, radius){
-        $scope.circle = new google.maps.Circle(
-            {
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35,
-                map: $scope.map,
-                center: new google.maps.LatLng(loc[0], loc[1]),
-                radius: radius * 1000
-            }
-        );
-    };
-
-    $scope.changeCircleRadius = function(radius){
-        $scope.circle.setRadius(radius * 1000);
-    };
-
-    $scope.getRadius = function(){
-        return $scope.circle.getRadius();
-    };
-
-    $scope.computeRoute = function()
-    {
-        var dep = new google.maps.LatLng($scope.loc[0], $scope.loc[1]);
-        var dest = new google.maps.LatLng($scope.defaultLocation[0], $scope.defaultLocation[1]);
-            var request = {
-                origin: dep,
-                destination: dest,
-                travelMode: google.maps.TravelMode.DRIVING,
-                unitSystem: google.maps.UnitSystem.METRIC
-            };
-
-            $scope.directionsService.route(request, function(result, status) {
-                if (status == google.maps.DirectionsStatus.OK) {
-                    $scope.directionsDisplay.setDirections(result);
-                    $scope.directionsDisplay.suppressMarkers = true;
-                    $scope.directionsDisplay.setOptions({
-                            markerOptions : google.maps.Animation.BOUNCE,
-                            preserveViewport : false
-                    });
-                }
-            });
-    };
+function mapController($scope, Map){
+    Map.displayMap();
 }
-
 /* Directives */
 
-myApp.directive('googlePlaces', function(){
+myApp.directive('googlePlaces', function(Map){
         return {
+
             // Remplace l'input google-places par un nouveau (dynamique, permettant l'autocomplete)
             restrict:'EA',
             replace:true,
@@ -177,14 +38,15 @@ myApp.directive('googlePlaces', function(){
 
 
                     // Chargement de la map
-                    $scope.changeLocation(loc);
-                    $scope.drawCircle(loc, 3);
-                    console.log($scope.getRadius());
-                    $scope.changeCircleRadius(0.5);
-                    console.log($scope.getRadius());
-                    $scope.computeRoute();
-                    // Applique les modification du scope et recharge les éléments modifiés
-                    $scope.$apply();
+                    Map.changeLocation(loc);
+                    Map.drawCircle(loc, 3);
+
+                    console.log(Map.getRadius());
+                    Map.changeCircleRadius(0.5);
+
+                    console.log(Map.getRadius());
+                    Map.computeRoute();
+                    Map.displayMap();
                 });
             }
         }
