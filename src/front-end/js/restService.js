c7bd63da1d0f52de19a5a4890bea7e3d32f77eb5
PLoic <loic.pauletto@gmail.com>
2014-12-17 12:13:41 +0100

    Add test function

--- src/front-end/js/restService.js
+++ src/front-end/js/restService.js
@@ -3,14 +3,28 @@ myApp.service('REST', function($http) {
     that = this;
 
     that.REST = function(method, part, data){
-
-        data = $.param(data);
-        return $http({
-            url: "http://localhost:8989/" + part,
-            method : method,
-            headers : {'Content-Type': 'application/x-www-form-urlencoded'},
-            data : data
+        that.testConnect().success(function(){
+            data = $.param(data);
+            alert('bonjour');
+            return $http({
+                url: "http://139.124.187.37:8989/" + part,
+                method: method,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                data: data
+            });
+        }).error(function(){
+            alert('BONJOUR');
         });
     };
+
+    that.testConnect = function() {
+        //alert('lel');
+        return $http({
+            url: "http://192.168.1.3:8989/",
+            method: 'GET',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            data: ' '
+        })
+    }
 
 });
