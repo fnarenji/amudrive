29766cd1addc3466d7a2c1df93aeacdfd12e3d5a
Thomas Munoz <thomas.munoz30@gmail.com>
2014-12-19 03:02:14 +0100

    Fix testConnect
    It works now

--- src/front-end/js/restService.js
+++ src/front-end/js/restService.js
@@ -1,12 +1,25 @@
-myApp.service('REST', function($http) {
+myApp.service('REST', function($http, $q, $timeout) {
 
     RestService = new Object();
 
     RestService.url = "http://192.168.0.31:8989/";
 
-    RestService.testConnect = function() {return $http.get(RestService.url);}
+    RestService.testConnect = function() {
+        var timeoutPromise = $timeout(function() {
+            canceler.resolve(); //aborts the request when timed out
+            alert("Le serveur n'a pas répondu à temps, veuillez recharger la page ou rééssayer plus tard.");
+        }, 500);
+
+        var canceler = $q.defer();
+        
+        $http.get(RestService.url + '/campuses').success(function(data){
+            $timeout.cancel(timeoutPromise);
+        });
+    }
 
     RestService.REST = function(method, part, data){
+        RestService.testConnect();
+
         // Clean parameters
         data = (data !== undefined) ? $.param(data) : '' ;
         part = (part !== undefined) ? part : '';
