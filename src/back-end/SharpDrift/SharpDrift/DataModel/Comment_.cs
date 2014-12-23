e430dd2ab23aba88a5ae9a186560f3caa812b838
sknz <florandara@gmail.com>
2014-12-23 23:57:09 +0100

    WIP

---
+++ src/back-end/SharpDrift/SharpDrift/DataModel/Comment_.cs
@@ -0,0 +1,12 @@
+﻿namespace SharpDrift.DataModel
+{
+    public class Comment_
+    {
+        public int IdCarPooling { get; set; }
+        public int IdClient { get; set; }
+        public int IdComment { get; set; }
+        public string Comment { get; set; }
+        public int PoolingMark { get; set; }
+        public int DriverMark { get; set; }
+    }
+}
--- src/front-end/js/carPoolings.js
+++ src/front-end/js/carPoolings.js
@@ -8,7 +8,20 @@ myApp.controller('CarPoolingController', ['$scope', 'mapService', function($scop
     $scope.smoke = false;
     $scope.talk = false;
     $scope.radio = false;
-    $scope.luggage = false
+    $scope.luggage = false;
+
+    $scope.campus = {};
+    REST.REST('GET','campuses')
+            .success(function(data){
+                $scope.campus = data;
+            });
+
+    $scope.getCampuses = function (id) {
+        for(var i = 0; i < $scope.campus.campuses.length; ++i){
+            if($scope.campus.campuses[i].idCampus === id)
+                return $scope.campus.campuses[i].name;
+        }
+    };
 
     mapService.setMarkerCallback(function (json) {
         console.log(json);
@@ -25,6 +38,7 @@ myApp.controller('CarPoolingController', ['$scope', 'mapService', function($scop
         $('#search-panel-select-luggage').css('background-image', 'url(http://localhost/amudrive/img/luggage_' + (json.luggage > 0 ? 'on' : 'off') + '.png)');
         $('#search-panel-date').html(json.meettime.substr('YYYY-'.length, 'MM-DD'.length).replace('-', '/') + ' ' + json.meettime.substr('YYYY-MM-DDT'.length, 'HH:MM'.length).replace(':', 'h'));
         $('#search-panel-depart').html('L\'adresse est ' + json.address);
-        $('#search-panel-arrival').html('Le pôle est ' + json.idCampus);
+        $('#search-panel-arrival').html('Le pôle est ' + $scope.getCampuses(json.idcampus));
+        $('#search-panel-passenger').html(json.room + ' places restantes');
     });
 }]);
